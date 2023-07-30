37882a8e427e4cd6355eb84d30b3c9eb124edd20
ajayparihar876 <ajayparihar876@gmail.com>
2023-07-30 20:22:34 +0530

    modified:   06.py

--- 06.py
+++ 06.py
@@ -1,9 +1,9 @@
-n = 20  # Number of rows
+n = 5  # Number of rows
 
 # Upper half of the diamond
 for i in range(n):
     print(' ' * (n - i) + '*' * (2 * i + 1))
 
 # Lower half of the diamond
-for i in range(n - 2, -1, -1):
+for i in range(n-2, -1, -1):
     print(' ' * (n - i) + '*' * (2 * i + 1))
